47d13d88ce764030c06dee816260f670103db789
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-23 13:55:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "ciao",
+            red: "red",
+            image: "img/ape.jpg"
+        }
+    }
+);
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
